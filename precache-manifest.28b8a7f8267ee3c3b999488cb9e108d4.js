71a5a0758a144856e3306b0e224d2d26eb174e95
David S <sawedavid7@gmail.com>
2022-04-19 11:03:18 +0100

    Updates

---
+++ precache-manifest.28b8a7f8267ee3c3b999488cb9e108d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "628353637dc2e0173d8c",
+    "url": "/cv-project/static/css/main.1b09ae33.chunk.css"
+  },
+  {
+    "revision": "628353637dc2e0173d8c",
+    "url": "/cv-project/static/js/main.62835363.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/cv-project/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "e495a233d13d271953c2",
+    "url": "/cv-project/static/js/2.e495a233.chunk.js"
+  },
+  {
+    "revision": "9af9f594b915193297a1",
+    "url": "/cv-project/static/js/runtime~main.9af9f594.js"
+  },
+  {
+    "revision": "cd2b3c29dc57dece1b458459ddd606a3",
+    "url": "/cv-project/index.html"
+  }
+];
